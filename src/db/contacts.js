4070001845a7c97da01f9f23cf6fb5cff98665b7
misko_16 <119707421+misko16@users.noreply.github.com>
2024-01-16 16:58:47 -0800

    x

---
+++ src/db/contacts.js
@@ -0,0 +1,57 @@
+const fs = require('fs/promises');
+const contactsPath = require('./contacts.json');
+
+let cachedContacts = null;
+
+async function listContacts() {
+  try {
+    if (!cachedContacts) {
+      const data = await fs.readFile(contactsPath, 'utf-8');
+      cachedContacts = JSON.parse(data);
+    }
+    return cachedContacts;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    return contacts.find(contact => contact.id === contactId);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const updatedContacts = contacts.filter(contact => contact.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+    cachedContacts = updatedContacts; 
+    return updatedContacts;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const newContact = { id: new Date().toISOString(), name, email, phone };
+    const contacts = await listContacts();
+    const updatedContacts = [...contacts, newContact];
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+    cachedContacts = updatedContacts; 
+    return newContact;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact
+};
--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,6 @@
 import { program } from "commander";
+const {listContacts, getContactById, removeContact, addContact, addContact} = require('../db/contacts');
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,27 +12,35 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
+
 async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      // ...
-      break;
-
-    case "get":
-      // ... id
-      break;
-
-    case "add":
-      // ... name email phone
-      break;
-
-    case "remove":
-      // ... id
-      break;
-
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+  try{
+    switch (action) {
+      case "list":
+        const contactsList = await listContacts();
+        console.log(`Contact list:`, contactsList);
+        break;
+  
+      case "get":
+        const findContact = await getContactById(id);
+        console.log(`Finding contacts:`, findContact);
+        break;
+  
+      case "add":
+        const contactsAdding = await addContact(name, email, phone);
+        console.log(`Adding contacts:`, contactsAdding);
+        break;
+  
+      case "remove":
+        const deleteContacts = await removeContact(id);
+        console.log(`Delete contact:`, deleteContacts);
+        break;
+  
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  }catch(error){
+    console.log(error.message);
   }
 }
 
